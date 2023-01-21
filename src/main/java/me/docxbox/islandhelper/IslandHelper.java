a2bd4daef74571be16979bee1cf9b515078c381d
hunter <watermelonmcmuffintv@gmail.com>
2023-01-21 17:46:44 -0500

    added README.md

--- src/main/java/me/docxbox/islandhelper/IslandHelper.java
+++ src/main/java/me/docxbox/islandhelper/IslandHelper.java
@@ -1,7 +1,5 @@
 package me.docxbox.islandhelper;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,9 +7,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class IslandHelper extends JavaPlugin {
 
-    // mainland information
+    // mainland world information
     private World mainland_world = Bukkit.getWorld("world");
-    private Location mainland = new Location(Bukkit.getWorld("world"), 0, 0, 0);
+    private Location mainland_location = new Location(
+            mainland_world,
+            mainland_world.getSpawnLocation().getX(),
+            mainland_world.getSpawnLocation().getY(),
+            mainland_world.getSpawnLocation().getZ()
+    );
 
     @Override
     public void onEnable() {
@@ -25,13 +28,14 @@ public final class IslandHelper extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // if player wants to go to the mainland
+        // if player wants to go to the mainland, teleport them to the world spawn
         if (command.getName().equalsIgnoreCase("mainland") && sender instanceof Player){
             Player p = (Player) sender;
-            p.teleport
+            p.sendMessage(ChatColor.GREEN + "Going to mainland...");
+            p.teleport(mainland_location);
+            mainland_world.playSound(p.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 10, 1);
+            mainland_world.playEffect(p.getLocation(), Effect.MOBSPAWNER_FLAMES, 0);
         }
-
-
         return true;
     }
 }
